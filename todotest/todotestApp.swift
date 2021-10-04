9b3e591bae2175cb48f57511a518ad57308b7c47
Luka <vujnovacluka89@gmail.com>
2021-10-04 21:51:00 +0200

    Initial Commit

---
+++ todotest/todotestApp.swift
@@ -0,0 +1,17 @@
+//
+//  todotestApp.swift
+//  todotest
+//
+//  Created by Luka Vujnovac on 04.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct todotestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
